070dbf4132024a4f6caea389541098cb1cfe0301
kasperlauge <kasperlaugemadsen@gmail.com>
2020-07-02 21:31:37 +0200

    Added test for N=512 and a scale factor

--- cs_config.h
+++ cs_config.h
@@ -1,9 +1,9 @@
 #ifndef CS_CONF_H_
 #define CS_CONF_H_
 
-#define M 150
+#define M 300
 #define BLOCK_LEN M*2
-#define N_CS 256
+#define N_CS 512
 #define EMAX 0x000f0000 /* */
 #ifndef LOG_MODULE
 #define LOG_MODULE "compressed_sensing"
